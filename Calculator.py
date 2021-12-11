31b47328c4fef9b2092673c765570ea6c2331cee
Lars Bösch <larsboesch@lars.local>
2021-12-11 16:37:30 +0100

    first calculations

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,18 @@
-from tkinter import *
-from GUI import *
 from Beverage import *
+from datetime import datetime
+
 
 def calculate_coffein(name):
     if name == "cappucino":
         return Beverage.ESRPESSO.value
     elif name == "doppio":
         return 2*Beverage.ESRPESSO.value
+    else:
+        return 80
 
-if __name__ == "__main__": 
-    window=Tk()
-    mywin=MyWindow(window)
-    window.title('Hello Python')
-    window.geometry("400x300+10+10")
-    window.mainloop()
+def calculate_Volume(weight, height):
+    return 51.44*weight / height + 15.3
+
+def calculate_Time():
+    now = datetime.now()
+    return now
--- GUI.py
+++ GUI.py
@@ -1,27 +1,57 @@
 from tkinter import *
+import Calculator
+
 class MyWindow:
     def __init__(self, win):
         self.lbl1=Label(win, text='Weight in kg')
         self.lbl2=Label(win, text='Height in cm')
-        self.lbl3=Label(win, text='Result')
-        self.t1=Entry(bd=3)
-        self.t2=Entry()
-        self.t3=Entry()
-        self.btn2=Button(win, text='Calculate')
-        self.lbl1.place(x=100, y=50)
-        self.t1.place(x=200, y=50)
-        self.lbl2.place(x=100, y=100)
-        self.t2.place(x=200, y=100)
-        self.b2=Button(win, text='Calculate')
-        self.b2.bind('<Button-1>', self.calc)
-        self.b2.place(x=200, y=150)
-        self.lbl3.place(x=100, y=200)
-        self.t3.place(x=200, y=200)
-    
-    def calc(self, event):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=51.44*num1 / num2 + 15.3
-        self.t3.insert(END, str(result))
+        self.lbl3=Label(win, text='Volume')
+        self.lbl4=Label(win, text='Beverage')
+        self.lbl5=Label(win, text='Estimated time')
 
+        self.weight=Entry(bd=3)
+        self.height=Entry()
+        self.volume=Entry()
+        self.beverage=Entry()
+        self.time=Entry()
+
+        self.lbl1.place(x=100, y=50)
+        self.weight.place(x=200, y=50)
+
+        self.lbl2.place(x=100, y=100)
+        self.height.place(x=200, y=100)
+
+        self.lbl4.place(x=100, y=150)
+        self.beverage.place(x=200, y=150)
+
+        self.lbl3.place(x=100, y=300)
+        self.volume.place(x=200, y=300)
+
+        self.lbl5.place(x=100, y=350)
+        self.time.place(x=200, y=350)
+
+        self.b1=Button(win, text='Calculate-Volume')
+        self.b1.bind('<Button-1>', self.calcVol)
+        self.b1.place(x=100, y=200)
+        self.b2=Button(win, text='Calculate-Time')
+        self.b2.bind('<Button-1>', self.calcTime)
+        self.b2.place(x=250, y=200)
+        
+
+    
+    def calcVol(self, event):
+        self.volume.delete(0, 'end')
+        result=Calculator.calculate_Volume(int(self.weight.get()), int(self.height.get()))
+        self.volume.insert(END, str(result))
+
+    def calcTime(self, event):
+        self.time.delete(0, 'end')
+        result=Calculator.calculate_Time()
+        self.time.insert(END, str(result))
+
+if __name__ == "__main__": 
+    window=Tk()
+    mywin=MyWindow(window)
+    window.title('Hello Python')
+    window.geometry("600x600+10+10")
+    window.mainloop()
